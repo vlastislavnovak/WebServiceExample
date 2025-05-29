353c3948ef99a4866cdbacb9f7e99ce164dae62b
Vlastislav Novák <vlastislav.novak@gmail.com>
2025-05-29 12:10:53 +0200

    Přidejte soubory projektu.

---
+++ WebServiceExample/Controllers/WeatherForecastController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebServiceExample.Controllers
+{
+    [ApiController]
+    [Route("api")]
+    public class WeatherForecastController : ControllerBase
+    {
+        public struct User
+        {
+            public string Name { get; set; }
+            public DateTime BirthDate { get; set; }
+        }
+
+        [HttpGet("test")]
+        public User test()
+        {
+            User user = new User();
+            user.Name = "Jan Novák";
+            user.BirthDate = DateTime.Now;
+            return user;
+        }
+
+        [HttpGet("test2")]
+        public String test2()
+        {
+            return "Ahoj svìte";
+        }
+    }
+}
